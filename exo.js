7c55a574e952d775273bf4e167d1aa5c56b68ed3
Zales13 <dxissa@gmail.com>
2021-02-23 15:40:04 +0100

    correction du prof

--- exo.js
+++ exo.js
@@ -2,6 +2,7 @@
 create script add that takes two string and returns a string and use console.log()
 to show the result. */
 
-let x = 9
-let y = 9
-console.log( y + x);
+let number1 = parseInt("9", 10)
+let number2 = parseInt("5", 10)
+
+console.log(typeof  `${number1 + number2}`);
