26f69deca170aad78763f7b9ed992f57528712bc
Zales13 <dxissa@gmail.com>
2021-02-23 15:45:34 +0100

    correction

--- exo.js
+++ exo.js
@@ -2,7 +2,19 @@
 create script add that takes two string and returns a string and use console.log()
 to show the result. */
 
+
 let number1 = parseInt("9", 10)
 let number2 = parseInt("5", 10)
 
 console.log(typeof  `${number1 + number2}`);
+console.log((number1 + number2).toString());
+
+
+/* Second step 
+Cree une fonction add (string)
+ - cette methode peux prendre 0, 1 ou 2 nombres separated by comma, and returns their sum.
+ - An empty string will return "0".
+ - Example of inputs "", "1", "1.1,2.2".
+ - The results of this addition "1.1,2.2" need to be "3.3" /!\
+ */
+
